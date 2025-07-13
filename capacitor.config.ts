575e7f473b941b3e156de5c739b8d838a7498ac0
johnneerdael <john@neerdael.nl>
2025-07-13 18:22:28 +0200

    fixes

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.yourcompany.focusmatrix',
+  appId: 'com.piehouse.focusmatrix',
   appName: 'FocusMatrix',
   webDir: 'dist',
 
