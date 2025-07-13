bc256d430d42f31227dbe4c0d65d73f7b18ce034
johnneerdael <john@neerdael.nl>
2025-07-13 23:40:31 +0200

    header

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,12 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.piehouse.focusmatrix',
   appName: 'FocusMatrix',
-  webDir: 'dist',
-
-  /* ▼ NEW ▼ */
-  server: {
-    url: 'https://pomodoro.thepi.es',   // the HTTPS origin that serves your PWA
-    cleartext: false                    // block accidental http:// loads
-  }
+  webDir: 'dist'
 };
+
 export default config;
